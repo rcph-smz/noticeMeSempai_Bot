69cb15f22e773bd9d57653b6cd87942b311ccbdc
rcph <rcph110110@gmail.com>
2022-06-29 15:59:13 +0800

    done

--- app.py
+++ app.py
@@ -1,128 +1,54 @@
+from dis import disco
+from turtle import color
 import discord
+from discord.ext import commands
 import os
 from random import randint
 from googletrans import Translator
 # import pymongo
 
-
-client = discord.Client()
-
+client = commands.Bot(command_prefix="./")
+client.remove_command("help")
 
 @client.event
 async def on_ready():
-    print("i have logged in as {0.user}".format(client))
-
-listOfSenpaiStatement = [
-  ["hey senpai","hmm? （；^ω^）"],
-  ["senpai","Did someone call Me Sempai? ૮₍ ˃ ⤙ ˂ ₎ა "],
-  ["you like me sempai","Of course I like You ˃ ⤙ ˂"],
-  ["sana all","sana all"],
-  ["how are you today sempai","dont worry im fine (⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄"],
-  ["did you eat your lunch sempai","Yup!!!, im already full (っˆڡˆς)"],
-  ["hey sempai, whats my gender","i dont know tehee ^m^"],
-  ["am i gay","Idk Touch Some Grass Tehee ^m^"],
-  ["i like loli","should i call fbi?( ノ ゜□ ゜)ノ"],
-  ["can i court you","Yes */blush"],
-  ["play","Play with Me (⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄"],
-  ["oh nyo","Oh Nyo ~ ＾º◡º＾"],
-  ["segs","Did somebody says Seggs? ૮ ˶ᵔ ᵕ ᵔ˶ ა"],
-  ["goodnight","_Goodnight_ ~ <3"],
-  ["cls","This is not Command Prompt (〃＾▽＾〃)"],
-  ["im sad","don't worry I'm here (〃＾▽＾〃)"],
-  ["uwu","_UwU_"],
-  ["notice me","OkayFine ૮₍ ˃ ⤙ ˂ ₎ა"],
-  ["yes","₍ ˃ ⤙ ˂ ₎"],
-  ["ohayou","_Ohayou!!!_ ~ ૮ ˶ᵔ ᵕ ᵔ˶ ა <3"],
-  ["oyasumi","_Oyasumi_ ~ ૮₍ ˃ ⤙ ˂ ₎ა <3"],
-  ['noticeme', 'OkayFine ૮₍ ˃ ⤙ ˂ ₎ა'],
-  ['nyt', 'GoodNight ~'],
-  ['i love you', 'I love you too  ~'],
-  ['yamete kudasai', '_Kyah_ ~  ~ YameeeTeee!!!!! _Ughhhh hmmp_'],
-  ['kawaii', '૮ ˶ᵔ ᵕ ᵔ˶ ა'],
-  ['kill me','Im not _"Yandere"_ _Hmmp_!!!']
-]
-listOfLinks = [
-  ["superidol","https://c.tenor.com/dIaP-9Yp9fIAAAAd/super-idol.gif"],
-  ['noticemesempai_link', 'https://discord.com/api/oauth2/authorize?client_id=964386726372065280&permissions=8&scope=bot']
-]
-listOfSenpaiResponse = [
-  ["?",["Yesss ~","HuHuHu!!!","_Ugghhh_","No...","_?_"]]
-]
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.lower().startswith("leave_server"):
-      try:
-        client_guild = client.get_guild(int(message.content[12:].strip()))
-        await client_guild.leave()
-      except:
-        pass
-
-    # help
-    if message.content.lower() == 'help':
-      await message.channel.send("""
-Help Me Sempai :  _Innocent command ૮₍ ˃ ⤙ ˂ ₎ა_  
-
-translate <language> - <wants to translate>
-
-leave_server <server id>
-
-say <say what you want>
-
-# I'm going go add more features
-
-# and this bot has a tonnes of bugs
-                                """)
-                                   
-
-  # * add<not_available> - adding your key-value preferences 
-  #   ex:
-  #     add <nameofkey> - <value>
-  #     add NoticeMe - OkayFine ૮₍ ˃ ⤙ ˂ ₎ა
-  #       // her response is OkayFine ૮₍ ˃ ⤙ ˂ ₎ა 
-  # * ? - ask sempai for question
-  #   ex: 
-  #     <question>?
-  #     can i court you? 
-  #       // her response might be "Yesss ~","HuHuHu!!!","_Ugghhh_","No...","_?_"
+  print(f'I have been login as {client.user}')
 
 
+get_list = ["guild_id","author","ping"]
 
+def get_command(ctx,k,i):
+  index = get_list.index(k)
+  ctx_list = [ctx.guild.id,ctx.author,f'{round(client.latency * 1000)}ms']
 
+  return ctx_list[index]
+  
+  
 
+@client.command()
+async def get(ctx, *args):
+  for i in args:
+    for k in get_list:
+      if k in i:
+        await ctx.send(get_command(ctx,k,i))
 
-    # translator
-      # pip install googletrans==4.0.0rc1
+@client.command()
+async def translate(ctx, language , *args):
+  await ctx.send(Translator().translate(" ".join(args),dest=language).text)
 
-    if(message.content.startswith("translate")):
-      await message.channel.send(Translator().translate(message.content[9:].strip().split("-")[1].strip(),dest=str(message.content[9:].strip().split("-")[0].strip())).text)
-     # await message.delete()
-      
-    # static question
-    for x in listOfSenpaiStatement:
-      if message.content.lower() in x:
-        await message.channel.send(x[1])
-    # play with sempai
-    for x in listOfSenpaiResponse:
-      if not message.content.lower().startswith("say") and not message.content.lower().startswith("translate") and message.content[message.content.find(x[0])] in x:
-        await message.channel.send(x[1][randint(0,int(len(x[1]))) - 1])
-    # links
-    for x in listOfLinks:
-      if message.content.lower() in x:
-        await message.channel.send(x[1])
-    #say <message>
-    if(message.content.lower().startswith("say")):
-      await message.channel.send(message.content[3:])
-      # await message.delete()
+@client.command()
+async def say(ctx,*message):
+  await ctx.send(" ".join(message))
+
+field_list = [["guild_id","./get guild_id"],["author","./get author"],["ping","./get ping"],["translate","./translate <language> <argument>"],["say","./say <argument/message>"]]
+@client.group(invoke_without_command=True)
+async def help(ctx):
+  em = discord.Embed(title = "commands", value = "use ./help command to see all stuff", color = discord.Color.from_rgb(198,175,165))
+
+  for flist in field_list:
+    em.add_field(name=flist[0],value=flist[1])
+
+  await ctx.send(embed = em)
+
         
-    # special xD
-       
-    if message.content == "Notice Me Senpai":
-      await message.channel.send("I want to Marry You, shall We? ૮₍ ˃ ⤙ ˂ ₎ა ")
-    if message.content == "what's your phone number":
-      await message.channel.send("09"+str(randint(0,10))+str(randint(0,10))+str(randint(0,10))+str(randint(0,10))+str(randint(0,10))+str(randint(0,10))+str(randint(0,10))+str(randint(0,10))+str(randint(0,10)))
-
 client.run("OTY0Mzg2NzI2MzcyMDY1Mjgw.GZPbzS.5TGr1VvoTwRoco9q8h1Mk-qNgNX2M2iKMGsBYI")
